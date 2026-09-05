daaed4122db8b3086a5fc0f775f9bb07636dcccc
aCoderChild <phammaianh11102005@gmail.com>
2026-09-05 15:26:50 +0000

    Revert "Strengthen JoyMedia generation data model"
    This reverts commit 3f37b4b79146d8086788844a56bbec5b3ade5683.

--- joymedia/joymedia/doctype/media_asset/media_asset.py
+++ joymedia/joymedia/doctype/media_asset/media_asset.py
@@ -7,16 +7,8 @@ from frappe.model.document import Document
 
 class MediaAsset(Document):
 	def validate(self):
-		if self.asset_scope == "Organization":
-			if not self.client_organization:
-				frappe.throw("Client Organization is required for Organization-scoped assets")
-			if self.media_project:
-				frappe.throw("Organization-scoped assets cannot be linked to a Media Project")
-		elif self.asset_scope == "Project":
-			if not self.media_project:
-				frappe.throw("Media Project is required for Project-scoped assets")
-			self.client_organization = frappe.db.get_value(
-				"Media Project", self.media_project, "client_organization"
-			)
-		else:
-			frappe.throw("Asset Scope must be Organization or Project")
+		if self.asset_scope == "Organization" and not self.client_organization:
+			frappe.throw("Client Organization is required for Organization-scoped assets")
+
+		if self.asset_scope == "Project" and not self.media_project:
+			frappe.throw("Media Project is required for Project-scoped assets")
