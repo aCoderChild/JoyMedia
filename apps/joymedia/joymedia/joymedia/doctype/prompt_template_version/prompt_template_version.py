14d6a8d240587b7e688d8c88c2c7a9378c114638
aCoderChild <phammaianh11102005@gmail.com>
2026-09-05 14:18:39 +0000

    Move JoyMedia source to canonical app path

--- apps/joymedia/joymedia/joymedia/doctype/prompt_template_version/prompt_template_version.py
+++ apps/joymedia/joymedia/joymedia/doctype/prompt_template_version/prompt_template_version.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2026, JoyMedia and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class PromptTemplateVersion(Document):
-	pass
+	def validate(self):
+		if not self.prompt_template or self.version_number is None:
+			return
+
+		filters = {"prompt_template": self.prompt_template, "version_number": self.version_number}
+		if self.name:
+			filters["name"] = ["!=", self.name]
+		if frappe.db.exists("Prompt Template Version", filters):
+			frappe.throw(_("Prompt Template Version Number must be unique within a Prompt Template."))
--- joymedia/joymedia/doctype/client_organization/client_organization.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, JoyMedia and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class ClientOrganization(Document):
-	pass
