de0672ed861529f76bd30a30616897aeaf217454
aCoderChild <phammaianh11102005@gmail.com>
2026-09-05 15:34:07 +0000

    Remove duplicate JoyMedia app tree

--- apps/joymedia/joymedia/joymedia/doctype/generation_input/generation_input.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, JoyMedia and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Generation Input", {
-// 	refresh(frm) {
-
-// 	},
-// });
