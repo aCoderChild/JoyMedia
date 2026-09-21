63d1df9ff4f5834c52be9fa9f7441f4d90d5cbe3
MaiAnhPham <167604421+aCoderChild@users.noreply.github.com>
2026-09-22 06:06:55 +0700

    add workflow profile desk controller

--- joymedia/joymedia/doctype/workflow_profile/workflow_profile.js
+++ joymedia/joymedia/doctype/workflow_profile/workflow_profile.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2026, JoyMedia and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Workflow Profile", {
-// 	refresh(frm) {
+frappe.ui.form.on("Workflow Profile", {
+	refresh(frm) {
+		if (frm.is_new()) return;
 
-// 	},
-// });
+		frm.add_custom_button(__("Workflow Versions"), () => {
+			frappe.set_route("List", "Workflow Version", {
+				workflow_profile: frm.doc.name,
+			});
+		});
+
+		frm.add_custom_button(__("New Workflow Version"), () => {
+			frappe.new_doc("Workflow Version", {
+				workflow_profile: frm.doc.name,
+			});
+		});
+
+		if (frm.doc.default_workflow_version) {
+			frm.add_custom_button(__("Open Default Workflow"), () => {
+				frappe.set_route(
+					"Form",
+					"Workflow Version",
+					frm.doc.default_workflow_version
+				);
+			});
+		}
+	},
+});
